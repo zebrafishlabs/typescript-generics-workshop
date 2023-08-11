9eaacc81197ca7fbd934b8a0d04953c91ae5475a
Luis Ball <luqven@gmail.com>
2023-08-11 10:39:01 -0700

    feat: complete exercise 03

--- src/01-generics-intro/03-multiple-generics.problem.ts
+++ src/01-generics-intro/03-multiple-generics.problem.ts
@@ -1,7 +1,7 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const returnBothOfWhatIPassIn = (a: unknown, b: unknown) => {
+const returnBothOfWhatIPassIn = <Ta, Tb>(a: Ta, b: Tb) => {
   return {
     a,
     b,
