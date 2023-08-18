1ff418b5764ec761fa62eacc0de79b1b91a702ae
Luis Ball <luqven@gmail.com>
2023-08-18 10:38:44 -0700

    feat: complete e-10

--- src/02-passing-type-arguments/10-reduce.problem.ts
+++ src/02-passing-type-arguments/10-reduce.problem.ts
@@ -10,7 +10,14 @@ const array = [
   },
 ];
 
-const obj = array.reduce((accum, item) => {
+// TS infers the type of array {name: string}[] but defaults the return type of
+// reduce to an empty object, {}.
+// This is because reduce infers the return type from the initial value, {}.
+// By passing Record<string, {name: string}> as a type argument, reduce can
+// correctly infer the return type.
+// We're making use of reduce's generic type arguments to give it a better idea
+// of what the correct return type is.
+const obj = array.reduce<Record<string, {name: string}>>((accum, item) => {
   accum[item.name] = item;
   return accum;
 }, {});
