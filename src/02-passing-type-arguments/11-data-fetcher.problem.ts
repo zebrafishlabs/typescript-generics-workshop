a9f474502493d24741666e97f61fbe1cc47819e0
Luis Ball <luqven@gmail.com>
2023-08-18 10:45:27 -0700

    feat: complete e-11

--- src/02-passing-type-arguments/11-data-fetcher.problem.ts
+++ src/02-passing-type-arguments/11-data-fetcher.problem.ts
@@ -1,8 +1,12 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const fetchData = async (url: string) => {
-  const data = await fetch(url).then((response) => response.json());
+// Adding a return type only gives us so much type safety. We can stull
+// re-assign data inside the body of the function and TS won't error out.
+// Instead we should set the type of data on the variable before the fetch call
+const fetchData = async <TData>(url: string) => {
+  const data:TData = await fetch(url).then((response) => response.json());
+  // now data can't be re-assigned to null
   return data;
 };
 
